758a86a8474403302896ae4839b61fb8ff1ec5a9
Cameron Ridgewell <cameron.ridgewell@gmail.com>
2022-02-28 20:11:41 -0500

    Began formatting to python function definitions

--- scraper.py
+++ scraper.py
@@ -12,56 +12,80 @@ import time
 import urllib.parse
 import csv
 
-print("generating options")
-chrome_options = Options()
-chrome_options.add_argument("--disable-extensions")
-#chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox") # linux only
-chrome_options.add_argument("--headless")
-chrome_options.headless = True # also works
+#email tools 
+import smtplib, ssl
 
-status = os.system('systemctl is-active --quiet chromium-driver')
-print(status)
+def createWebDriver():
+    print("generating options")
+    chrome_options = Options()
+    # ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+    #AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); # https:#www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+    chrome_options.add_argument("start-maximized"); # https:#stackoverflow.com/a/26283818/1689770
+    chrome_options.add_argument("enable-automation"); # https:#stackoverflow.com/a/43840128/1689770
+    chrome_options.add_argument("--headless"); # only if you are ACTUALLY running headless
+    chrome_options.add_argument("--no-sandbox"); #https:#stackoverflow.com/a/50725918/1689770
+    chrome_options.add_argument("--disable-infobars"); #https:#stackoverflow.com/a/43840128/1689770
+    chrome_options.add_argument("--disable-dev-shm-usage"); #https:#stackoverflow.com/a/50725918/1689770
+    chrome_options.add_argument("--disable-browser-side-navigation"); #https:#stackoverflow.com/a/49123152/1689770
+    chrome_options.add_argument("--disable-gpu"); #https:#stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
 
-print("setting up options")
-driver = webdriver.Chrome(options=chrome_options)
+    status = os.system('systemctl is-active --quiet chromium-driver')
+    print(status)
 
-#url of the page we want to scrape
-with open('bottle_list.csv','r', newline='') as csvfile:
-    bottle_list = csv.DictReader(csvfile)
+    print("setting up options")
+    return webdriver.Chrome(options=chrome_options)
 
-#with open('bottle_list.csv','w', newline='') as csvfile_out:
-#    fieldnames = ['title','stock']
-#    output_bottle_list = csv.DictWriter(csvfile,fieldnames=fieldnames)
-#    output_bottle_list.writeheader()
+def isInStock(bottle_name):
+    url = "https://www.abc.virginia.gov/products/all-products#q=" + urllib.parse.quote(bottle_name) + "&sort=relevancy&numberOfResults=12&f:Views=[49]"
+    driver.get(url) 
 
-    for row in bottle_list:
-#        print(row['title'])
-        url = "https://www.abc.virginia.gov/products/all-products#q=" + urllib.parse.quote(row['title']) + "&sort=relevancy&numberOfResults=12&f:Views=[49]"
-#        print(url)
-        driver.get(url) 
+    # this is just to ensure that the page is loaded
+    time.sleep(5) 
 
-# this is just to ensure that the page is loaded
-        time.sleep(5) 
+    html = driver.page_source
 
-        html = driver.page_source
+    # this renders the JS code and stores all
+    # of the information in static HTML code.
 
-        # this renders the JS code and stores all
-        # of the information in static HTML code.
-  
-        # Now, we could simply apply bs4 to html variable
-        soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-        search_results = soup.find_all('div',class_="col-sm-12 col-xs-8 pull-right-sm product-header")
+    search_results = soup.find_all('div',class_="col-sm-12 col-xs-8 pull-right-sm product-header")
 
-        stock_status = ""
-        print(row['title'].capitalize(), end=': ')
-        if len(search_results) > 0:
-            print("In Stock")
-            stock_status = "In Stock"
-        else:
-            print("Out of Stock")
-            stock_status = "Out of Stock"
+    if len(search_results) > 0:
+        return True
+    else:
+        return False
 
-driver.close() # closing the webdriver
-driver.quit()
+def sendEmail(body):
+    with open("secrets.txt") as f:
+        lines = f.readlines()
+        username = lines[0].strip()
+        password = lines[1].strip()
+        print(f"USERNAME={username}, PASSWORD={password}")
+        port = 465  # For SSL
+
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+            server.login(username, password)
+            message = body
+            server.sendmail(username, username, message)
+
+if __name__ == "__main__":
+    # sendEmail("hello")
+    with open('bottle_list.csv','r', newline='') as csvfile:
+        bottle_list = csv.DictReader(csvfile)
+
+        driver = createWebDriver()
+
+        for row in bottle_list:
+            stockStatus = "In Stock" if isInStock(row['title']) else "Out of Stock"
+            print(row['title'].title() + ": " + stockStatus)
+
+
+    driver.close() # closing the webdriver
+    driver.quit()
+
+
+
