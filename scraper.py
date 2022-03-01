b71a78d55feeb7691af103a1719eea1f3ae526d0
Cameron Ridgewell <cameron.ridgewell@gmail.com>
2022-03-01 08:54:21 -0500

    Completed email, added status messages

--- scraper.py
+++ scraper.py
@@ -16,8 +16,12 @@ import csv
 import smtplib, ssl
 from email.mime.text import MIMEText
 
+CSV_FILENAME = 'bottle_list.csv'
+SECRETS_FILENAME = 'secrets.txt'
+URL_PREFIX = 'https://www.abc.virginia.gov/products/all-products#q='
+URL_SUFFIX = '&sort=relevancy&numberOfResults=12&f:Views=[49]'
+
 def createWebDriver():
-    print("generating options")
     chrome_options = Options()
     # ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
     #AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); # https:#www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
@@ -31,14 +35,15 @@ def createWebDriver():
     chrome_options.add_argument("--disable-gpu"); #https:#stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
 
     status = os.system('systemctl is-active --quiet chromium-driver')
-    print(status)
+    # print(status)
 
-    print("setting up options")
+    print("Setting up options")
     return webdriver.Chrome(options=chrome_options)
 
 def isInStock(bottle_name):
-    url = "https://www.abc.virginia.gov/products/all-products#q=" + urllib.parse.quote(bottle_name) + "&sort=relevancy&numberOfResults=12&f:Views=[49]"
-    driver.get(url) 
+    url = URL_PREFIX + urllib.parse.quote(bottle_name) + URL_SUFFIX
+    print("Fetching " + bottle_name + "... ", end='')
+    driver.get(url)
 
     # this is just to ensure that the page is loaded
     time.sleep(5) 
@@ -51,14 +56,21 @@ def isInStock(bottle_name):
     soup = BeautifulSoup(html, "html.parser")
 
     search_results = soup.find_all('div',class_="col-sm-12 col-xs-8 pull-right-sm product-header")
-
+    
+    print("Done")
     if len(search_results) > 0:
         return True
     else:
         return False
 
+def writeStocktoFile(bottle_stock):
+    f = open(CSV_FILENAME, "w")
+    f.write(bottle_stock)
+    f.close()
+
+
 def sendEmail(body):
-    with open("secrets.txt") as f:
+    with open(SECRETS_FILENAME) as f:
         lines = f.readlines()
         username = lines[0].strip()
         password = lines[1].strip()
@@ -81,19 +93,21 @@ if __name__ == "__main__":
     driver = createWebDriver()
     bottle_list_output = ''
 
-    with open('bottle_list.csv','r', newline='') as csvfile:
+    with open(CSV_FILENAME,'r', newline='') as csvfile:
         bottle_list_input = csv.DictReader(csvfile)
         
         fieldnames = ['title','stock']
         bottle_list_output = fieldnames[0] + ',' + fieldnames[1] + '\n'
         
+        # Only send emails for things that were previously out of Stock
         for row in bottle_list_input:
             stockStatus = 'In Stock' if isInStock(row[fieldnames[0]]) else 'Out of Stock'
             if (row['stock'] == 'Out of Stock') and (stockStatus == 'In Stock'):
                 sendEmail(row['title'] + " is In Stock!")
+                pass
             bottle_list_output = bottle_list_output + row['title'] + ',' + stockStatus + '\n'
-
-    print(bottle_list_output)
+        #Updae CSV file
+        writeStocktoFile(bottle_list_output)
 
     driver.close() # closing the webdriver
     driver.quit()
